d63f1086a7396f6331d08cd8520aff661a23d3dc
codehlb <2396018503@qq.com>
2023-10-26 18:26:08 +0800

    test-10.26  LinkedList puke

--- test-10.26/src/Demo1.java
+++ test-10.26/src/Demo1.java
@@ -1,12 +1,74 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author: code_hlb
  * @date :  2023/10/26 11:14
- * @desc :  test: 给出一个有序的整数数组 A 和有序的整数数组 B ，请将数组 B 合并到数组 A 中，变成一个有序的升序数组
+ * @desc :  牛客刷题
  */
 public class Demo1 {
 
+    /* test: 读取字符串，中间以空格隔开，返回最后一个字符串的长度 */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        while (in.hasNext()) { // 注意 while 处理多个 case
+            String str = in.nextLine();
+
+            if(str.length() < 5000){
+                String[] newStr = str.split(" ");
+
+                String last = newStr[newStr.length - 1];
+                System.out.print(last.length());
+
+            }
+        }
+    }
+
+    /*   test: 给定一个长度为 n 的字符串，请编写一个函数判断该字符串是否回文。如果是回文请返回true，否则返回false。
+     *         并且字符串长度不大于1000000，且仅由小写字母组成
+     *         示例1
+     *         输入  ："absba"
+     *         返回值：true
+     * */
+    public static boolean judge (String str) {
+        // write code here
+        if (str.length() <= 1000000){
+            if (str.length() == 1){
+                return true;
+            }else {
+                int left = 0;
+                int right = str.length() - 1;
+                while (left <= right){
+
+                    Character ch1 = str.charAt(left);
+                    char ch2 = str.charAt(right);
+
+                    if (ch1 <= 'Z' || ch2 <= 'Z'){
+                        return false;
+                    }else if (ch1.equals(ch2)){
+                        left++;
+                        right--;
+                    }else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+
+        // 相当简洁！！
+        // return str.equals(new StringBuffer(str).reverse().toString());
+    }
+
+    public static void main2(String[] args) {
+        System.out.println(judge("dsadsa"));
+        System.out.println(Integer.valueOf('A'));  // 65
+        System.out.println(Integer.valueOf('a'));  // 97
+    }
+
+    /* test: 给出一个有序的整数数组 A 和有序的整数数组 B ，请将数组 B 合并到数组 A 中，变成一个有序的升序数组*/
     public static int[] merge(int A[], int m, int B[], int n) {
 
         // i从A[]的有效数组最后一位往前遍历
@@ -42,7 +104,7 @@ public class Demo1 {
         return A;
     }
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
 
         int[] arr1 = {4,5,6,0,0,0};
         int[] arr2 = {1,2,3};
--- test-10.26/src/Demo2.java
+++ test-10.26/src/Demo2.java
@@ -1,49 +1,53 @@
+
 /**
  * @author: code_hlb
- * @date :  2023/10/26 11:42
- * @desc :  test: 给定一个长度为 n 的字符串，请编写一个函数判断该字符串是否回文。如果是回文请返回true，否则返回false。
- *                并且字符串长度不大于1000000，且仅由小写字母组成
- *          示例1
- *          输入  ："absba"
- *          返回值：true
+ * @date :  2023/10/26 13:59
+ * @desc :  链表
  */
 public class Demo2 {
 
-    public static boolean judge (String str) {
-        // write code here
-
-        if (str.length() <= 1000000){
-            if (str.length() == 1){
-                return true;
-            }else {
-                int left = 0;
-                int right = str.length() - 1;
-                while (left <= right){
-
-                    Character ch1 = str.charAt(left);
-                    char ch2 = str.charAt(right);
-
-                    if (ch1 <= 'Z' || ch2 <= 'Z'){
-                        return false;
-                    }else if (ch1.equals(ch2)){
-                        left++;
-                        right--;
-                    }else {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        }
-        return false;
-
-        // 相当简洁！！
-        // return str.equals(new StringBuffer(str).reverse().toString());
-    }
-
     public static void main(String[] args) {
-        System.out.println(judge("dsadsa"));
-        System.out.println(Integer.valueOf('A'));  // 65
-        System.out.println(Integer.valueOf('a'));  // 97
+
+        MySingleList mySingleList = new MySingleList();
+        mySingleList.createList();
+
+        System.out.println("=========display()==========");
+        mySingleList.display();
+
+        System.out.println("=========size()==========");
+        System.out.println(mySingleList.size());
+
+        System.out.println("=========contains(3)==========");
+        System.out.println(mySingleList.contains(3));
+
+        System.out.println("=========addFirst()==========");
+        mySingleList.addHead(10);
+        mySingleList.addHead(9);
+        mySingleList.display();
+
+        System.out.println("=========addEnd(100)==========");
+        mySingleList.addtail(100);
+        mySingleList.display();
+
+        System.out.println("=========addByIndex()==========");
+        mySingleList.addByIndex(2,20);
+        mySingleList.addByIndex(0,8);
+        mySingleList.display();
+
+        System.out.println("=========delHead()==========");
+        mySingleList.delHead();
+        mySingleList.display();
+
+        System.out.println("=========delTail()==========");
+        mySingleList.delTail();
+        mySingleList.display();
+
+        System.out.println("=========delByKey(10)==========");
+        mySingleList.delByKey(10);
+        mySingleList.display();
+
+        System.out.println("=========delByIndex(1)==========");
+        mySingleList.delByIndex(1);
+        mySingleList.display();
     }
 }
