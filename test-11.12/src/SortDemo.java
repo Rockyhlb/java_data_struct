4fe408f71e7ba188b3d1ac0e3f8e7bf81a78a7bb
codehlb <2396018503@qq.com>
2023-11-13 17:19:54 +0800

    test-11.12 Sort

--- test-11.12/src/SortDemo.java
+++ test-11.12/src/SortDemo.java
@@ -1,17 +1,20 @@
+
 /**
- * @author: code_hlb
- * @date :  2023/11/12 13:51
- * @desc :  排序算法
+ * @BelongsProject: test-11.12
+ * @BelongsPackage: PACKAGE_NAME
+ * @CreateTime : 2023-11-13 14:58
+ * @Description: 排序算法
+ * @Author: code_hlb
  */
 public class SortDemo {
     /**
      * 1、直接插入排序：类似于打扑克插入牌的方法，拿到一张牌后依次与前面的元素比较，大则插入，小则继续往前比
-     *   时间复杂度：
-     *     最好情况：数据完全有序的时候：O(n)
-     *     最坏情况：数据完全逆序的时候：O(n^2)
-     *   空间复杂度：O(1)
-     *   稳定性(排序以后相同的元素顺序基于排序前不变)：稳定
-     *      *** 一个本身就是稳定的排序，是可以实现为不稳定的排序的 ***
+     *    时间复杂度：
+     *      最好情况：数据完全有序的时候：O(n)
+     *      最坏情况：数据完全逆序的时候：O(n^2)
+     *    空间复杂度：O(1)
+     *    稳定性(排序以后相同的元素顺序基于排序前不变)：稳定
+     *      *** 一个本身就是稳定的排序，是可以实现为不稳定的排序的     ***
      *      *** 但是一个本身就不稳定的排序，是不可能实现为稳定的排序的 ***
      */
     public void insertSort(int[] arrays) {
@@ -30,10 +33,10 @@ public class SortDemo {
     }
 
     /**
-     * 2、希尔排序： 是对直接插入排序的优化，通过采用跳跃式分组，再将每组进行插入排序，可能会将更小的元素尽可能往前放
-     *   时间复杂度: O(n^1.3 - n^1.5)
-     *   空间复杂度：O(1)
-     *   稳定性   ：不稳定
+     * 2、希尔排序  ：是对直接插入排序的优化，通过采用跳跃式分组，再将每组进行插入排序，可能会将更小的元素尽可能往前放
+     *    时间复杂度: O(n^1.3 - n^1.5)
+     *    空间复杂度：O(1)
+     *    稳定性   ：不稳定
      */
     public void shellSort(int[] arrays) {
         int gap = arrays.length;
@@ -62,10 +65,11 @@ public class SortDemo {
     }
 
     /**
-     * 3、直接选择排序： 每一次从待排序数据元素中选出最小(或最大)的一个元素，存放在序列的起始位置，直到全部待排序的数据元素排完
-     *   时间复杂度: 无论好坏情况,复杂度都是O(n)
-     *   空间复杂度：O(1)
-     *   稳定性   ：不稳定
+     * 3、选择排序(相对不实用)： 每一次从待排序数据元素中选出最小(或最大)的一个元素，
+     *                         存放在序列的起始位置，直到全部待排序的数据元素排完
+     *    时间复杂度: 无论好坏情况,复杂度都是O(n)
+     *    空间复杂度：O(1)
+     *    稳定性   ：不稳定
      */
     public void selectSort(int[] arrays) {
         for (int i = 0; i < arrays.length; i++) {
@@ -78,10 +82,141 @@ public class SortDemo {
             swap(arrays,minIndex,i);
         }
     }
-
-    public void swap(int[] arrays,int i,int j) {
+    public static void swap(int[] arrays,int i,int j) {
         int tmp = arrays[i];
         arrays[i] = arrays[j];
         arrays[j] = tmp;
+    }
+    // 选择排序的第二种写法，时间复杂度不变
+    public void selectSort2(int[] arrays) {
+        int left = 0;
+        int right = arrays.length - 1;
+        while (left < right) {
+            // 记录最小和最大下标
+            int minIndex = left;
+            int maxIndex = left;
+            for (int i = left + 1; i < right; i++) {
+                if (arrays[i] < arrays[minIndex]) {
+                    minIndex = i;
+                }else if (arrays[i] > arrays[maxIndex]){
+                    maxIndex = i;
+                }
+            }
+            swap(arrays,left,minIndex);
+            // 若最大值刚好在原来最小值的位置，此时已经被交换到了minIndex的位置，需要手动修正最大值的最新索引
+            if (maxIndex == left) {
+                maxIndex = minIndex;
+            }
+            swap(arrays,right,maxIndex);
+            left++;
+            right--;
+        }
+    }
+
+    /**
+     * 4、堆 排 序 ：通过创建大根堆，不断将堆顶元素与最后一个元素交换，再对其进行向下调整，最后结果就是从小到大的排序
+     *    时间复杂度: O(N*logN)  -->   比插入，选择，希尔排序更快
+     *               数据量很大时，堆排序 一定 比希尔排序快
+     *    空间复杂度：O(1)
+     *    稳定性   ：不稳定的
+     */
+    public void heapSort(int[] arrays) {
+        createBigHeap(arrays);
+        int end = arrays.length - 1;
+        while (end > 0) {
+            swap(arrays,0,end);
+            siftDown(arrays,0,end);
+            end--;
+        }
+    }
+    // 建立一个大根堆
+    private static void createBigHeap(int[] arrays) {
+        for (int parent = (arrays.length-1-1)/2; parent >= 0; parent--) {
+            siftDown(arrays,parent,arrays.length);
+        }
+    }
+    // 向下调整
+    private static void siftDown(int[] arrays,int parent,int end) {
+        int child = 2 * parent + 1;
+
+        while (child < end) {
+            if (child + 1 < end && arrays[child] < arrays[child + 1]) {
+                child++;
+            }
+            if (arrays[parent] < arrays[child]) {
+                swap(arrays,parent,child);
+                parent = child;
+                child = 2 * child + 1;
+            }else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 5、冒泡排序 ： 每一次将最大元素移动到最右端
+     *    时间复杂度: O(N^2) 如果加了优化，最好情况O(N)
+     *    空间复杂度：O(1)
+     *    稳定性   ：不稳定
+     */
+    public void bubbleSort(int[] arrays) {
+        for (int i = 0; i < arrays.length - 1; i++) {
+            boolean flag = false;
+            for (int j = 0; j < arrays.length-1-i; j++) {
+                if (arrays[j] > arrays[j + 1]) {
+                    swap(arrays,j,j+1);
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                return;
+            }
+        }
+    }
+
+    /**
+     * 6、快速排序  : 本质是二叉树，
+     *    时间复杂度
+     *        最好情况：O(N*logN)   -->  (满二叉树或完全二叉树)
+     *        最坏情况：O(N^2)      -->  (单分支树)
+     *    空间复杂度
+     *        最好情况：O(logN)     -->  (满二叉树或完全二叉树)
+     *        最好情况：O(N)        -->  (单分支树)
+     *    稳定性   : 不稳定
+     */
+    public void quickSort(int[] arrays) {
+        quick(arrays,0,arrays.length - 1);
+    }
+
+    private static void quick(int[] arrays,int start,int end) {
+        // 左边是一个节点或一个节点都没有时
+        if (start >= end) {
+            return;
+        }
+        int pivot = partition(arrays,start,end);
+        quick(arrays,start,pivot - 1);
+        quick(arrays,pivot + 1,end);
+    }
+    // 找基准
+    private static int partition(int[] arrays,int left,int right) {
+        int key = arrays[left];
+        // 记录最初left的位置
+        int i = left;
+        while (left < right) {
+            // 单独的一重循环，因此还要限制right的下标访问，最终right一定是比 key 小的值
+            while (left < right && arrays[right] >= key) {
+                right--;
+            }
+            // 最后left一定是比 key 大的值
+            while (left < right && arrays[left] <= key) {
+                left++;
+            }
+            // 交换left 和 right
+            swap(arrays,left,right);
+        }
+        // 将left 和 right相遇的位置 与 原来的基准交换
+        swap(arrays,left,i);
+        // 此时的left就是新的基准
+        return left;
     }
 }
--- test-11.12/src/Test.java
+++ test-11.12/src/Test.java
@@ -1,11 +1,13 @@
-import javax.xml.transform.sax.SAXSource;
+
 import java.util.Arrays;
 import java.util.Random;
 
 /**
- * @author: code_hlb
- * @date :  2023/11/12 12:49
- * @desc :  测试类
+ * @BelongsProject: test-11.12
+ * @BelongsPackage: PACKAGE_NAME
+ * @CreateTime : 2023-11-13 14:58
+ * @Description: 测试类
+ * @Author: code_hlb
  */
 public class Test {
 
@@ -106,13 +108,71 @@ public class Test {
         System.out.println("随机数组选择排序运行时间" + (endTime2 - startTime2));
     }
 
+    public static void testHeapSort(int[] array) {
+        SortDemo sortDemo = new SortDemo();
+        // 拷贝传入参数，避免对原数组进行数据的更改，防止后续测试其它排序算法时出现数据不一致
+        int[] tmp = Arrays.copyOf(array,array.length);
+
+        int[] arrays1 = orderArray(tmp);
+        long startTime = System.currentTimeMillis();
+        sortDemo.heapSort(arrays1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("有序数组堆排序运行时间" + (endTime - startTime));
+
+        int[] arrays2 = notOrderArray(tmp);
+        long startTime1 = System.currentTimeMillis();
+        sortDemo.heapSort(arrays2);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("逆序数组堆排序运行时间" + (endTime1 - startTime1));
+
+        int[] arrays3 = randomOrderArray(tmp);
+        long startTime2 = System.currentTimeMillis();
+        sortDemo.heapSort(arrays3);
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("随机数组堆排序运行时间" + (endTime2 - startTime2));
+    }
+
+    public static void testQuickSort(int[] array) {
+        SortDemo sortDemo = new SortDemo();
+        // 拷贝传入参数，避免对原数组进行数据的更改，防止后续测试其它排序算法时出现数据不一致
+        int[] tmp = Arrays.copyOf(array,array.length);
+
+        int[] arrays1 = orderArray(tmp);
+        long startTime = System.currentTimeMillis();
+        sortDemo.quickSort(arrays1);
+        long endTime = System.currentTimeMillis();
+        System.out.println("有序数组快速排序运行时间" + (endTime - startTime));
+
+        int[] arrays2 = notOrderArray(tmp);
+        long startTime1 = System.currentTimeMillis();
+        sortDemo.quickSort(arrays2);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("逆序数组快速排序运行时间" + (endTime1 - startTime1));
+
+        int[] arrays3 = randomOrderArray(tmp);
+        long startTime2 = System.currentTimeMillis();
+        sortDemo.quickSort(arrays3);
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("随机数组快速排序运行时间" + (endTime2 - startTime2));
+    }
+
     public static void main(String[] args) {
         int[] arrays = new int[100000];
+
+        int[] nums = {1,21,3,22,12,2};
+        SortDemo sortDemo = new SortDemo();
+        sortDemo.quickSort(nums);
+        System.out.println();
         System.out.println("=========直接插入排序==============");
         testInsertSort(arrays);
         System.out.println("=========希尔排序==============");
         testShellSort(arrays);
         System.out.println("=========选择排序==============");
         testSelectSort(arrays);
+        System.out.println("=========堆排序==============");
+        testHeapSort(arrays);
+        System.out.println("=========快速排序==============");
+        // 完全逆序的时候，空间复杂度为O(n)，因此会出现栈溢出异常
+        testQuickSort(arrays);
     }
 }
