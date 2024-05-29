ff0a178588e32f21efb7fd0e3405fb0da10bf000
codehlb <2396018503@qq.com>
2024-05-29 11:12:15 +0800

    test-20240529 exercises

--- exercises/src/dp/Demo7.java
+++ exercises/src/dp/Demo7.java
@@ -32,7 +32,43 @@ public class Demo7 {
      */
     public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
-        int[] dp = new int[m];
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        // 初始化第一行
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                // 如果碰到障碍物则退出循环，后续都默认为0
+                break;
+            }
+            // 初始化为能到达的路径为1
+            dp[0][i] = 1;
+        }
+        // 初始化第一列
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                // 如果碰到障碍物则退出循环，后续都默认为0
+                break;
+            }
+            dp[i][0] = 1;
+        }
+        // 开始遍历路径
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    // 当前路径依赖于上一行和上一列的数据
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static int uniquePathsWithObstacles1(int[][] obstacleGrid) {
+        // 滚动数组优化
+        int n = obstacleGrid[0].length;
+        int[] dp = new int[n];
         // 避免起始点就是障碍物
         dp[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
         for (int[] elem : obstacleGrid) {
@@ -43,16 +79,19 @@ public class Demo7 {
                     continue;
                 }
                 if (j - 1 >= 0 && elem[j - 1] == 0) {
+                    // 滚动数组思想：是一种能够在动态规划中降低空间复杂度的方法，有时某些二维dp方程可以直接降阶到一维
+                    // 通过观察dp方程来判断需要使用哪些数据，可以抛弃哪些数据，一旦找到关系，就可以用新的数据不断覆盖旧的数据量来减少空间的使用
                     dp[j] += dp[j - 1];
                 }
             }
         }
-        return dp[m - 1];
+        return dp[n - 1];
     }
 
     public static void main(String[] args) {
         //  输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
         int[][] obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
-        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));  // 2
+        System.out.println(uniquePathsWithObstacles1(obstacleGrid)); // 2
     }
 }
